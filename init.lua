aece408d12ceee23213cab708ad8067d865eeb4b
Silvan Schmidt <sil.schmidt@proton.me>
2023-06-17 16:32:21 +0200

    init

---
+++ init.lua
@@ -0,0 +1,12 @@
+return {
+    mappings = {
+        n = {
+            ["<C-Left>"] = "<C-w><Left>",
+            ["<C-Right>"] = "<C-w><Right>",
+            ["<C-Up>"] = "<C-w><Up>",
+            ["<C-Down>"] = "<C-w><Down>",
+            ["<C-h>"] = "<C-w>s",
+            ["<C-v>"] = "<C-w>v",
+        }
+    }
+}
